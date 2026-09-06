7735c2fddeaf329ab8003d1124dc8a12f09861d2
Omkar Phalle <omkarphalle77@gmail.com>
2026-09-06 19:15:24 +0530

    Time: 0 ms (100%), Space: 44.1 MB (11.49%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,32 +1,31 @@
 class Solution {
-    public int binarySearch(int[]nums,int low,int high,int target,int idx){
-        int n = nums.length;
+    public int rotatedBS(int[]nums,int low,int high,int target){
         while(low<=high){
             int mid = low + (high-low)/2;
             if(nums[mid]==target){
-                return (mid+idx)%n;
+                return mid;
             }
-            else if(nums[mid]<target){
-                low = mid + 1;
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && nums[mid]>target){
+                    high = mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
             }
             else{
-                high = mid - 1;
+                if(nums[high]>=target && nums[mid]<target){
+                    low = mid+1;
+                }
+                else{
+                    high = mid -1;
+                }
             }
-            
         }
         return -1;
     }
     public int search(int[] nums, int target) {
         int n = nums.length;
-        int mini = nums[0];
-        int idx = 0;
-        for(int i=1;i<n;i++){
-            if(nums[i]<mini){
-                mini = nums[i];
-                idx = i;
-            }
-        }
-        Arrays.sort(nums);
-        return binarySearch(nums,0,n-1,target,idx);
+        return rotatedBS(nums,0,n-1,target);
     }
 }

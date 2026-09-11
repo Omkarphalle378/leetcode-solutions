44004239763ca331bc44a993d46eff215ec1b75c
Omkar Phalle <omkarphalle77@gmail.com>
2026-09-11 10:25:03 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.29%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    int[] dp = new int[46];
+    public int helper(int n){
+        if(n==0) return 1;
+        if(n<0) return 0;
+
+        if(dp[n] !=0) return dp[n];
+        return dp[n] = helper(n-1) + helper(n-2);
+    }
+    public int climbStairs(int n) {
+        return helper(n);
+    }
+}

4aafd256f45faa0d61f35be52a16f67e03f0bbb0
Omkar Phalle <omkarphalle77@gmail.com>
2026-09-11 12:19:55 +0530

    Time: 60 ms (5.47%), Space: 138 MB (6.31%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,23 +1,27 @@
 class Solution {
+    int[][] dp = new int[1001][1001];
+    public int helper(String s, String t, int l, int r) {
+        if (r == t.length())
+            return 1;
+        if (l >= s.length())
+            return 0;
+
+        if (dp[l][r] != -1)
+            return dp[l][r];
+
+        int take = 0;
+        int skip = 0;
+        if (s.charAt(l) == t.charAt(r)) {
+            take = helper(s, t, l + 1, r + 1);
+        }
+        skip = helper(s, t, l + 1, r);
+        return dp[l][r] = take + skip;
+    }
+
     public int numDistinct(String s, String t) {
-        int n = s.length();
-        int m = t.length();
-
-        int[][] dp = new int[n+1][m+1];
-
-        //Make the last column 1 beacuase we can make the empty string using any number of character
-        for(int i=0;i<=n;i++){
-            dp[i][m] = 1;
+        for(int[]row : dp){
+            Arrays.fill(row,-1);
         }
-        for(int i=n-1;i>=0;i--){
-            for(int j=m-1;j>=0;j--){
-                dp[i][j] = dp[i+1][j];
-
-                if(s.charAt(i)==t.charAt(j)){
-                    dp[i][j]+=dp[i+1][j+1];
-                }
-            }
-        }
-        return dp[0][0];
+        return helper(s,t,0,0);
     }
 }

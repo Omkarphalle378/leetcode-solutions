2985eb3f72d229127fa0bc74077a8f1614229f74
Omkar Phalle <omkarphalle77@gmail.com>
2026-09-13 08:36:53 +0530

    Time: 12 ms (26.49%), Space: 50.2 MB (37.86%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,37 +1,26 @@
 class Solution {
     public boolean findCapacity(int[] nums,int days,int val){
-        int sum = 0;
-        int count = 1;
+        int sum = 0,count = 1;
         for(int i=0;i<nums.length;i++){
             if(sum + nums[i] > val){
                 count ++;
                 sum = nums[i];
             }  
-            else{
-                sum += nums[i];
-            }
+            else sum += nums[i];
         }
         return count<=days;
     }
     public int shipWithinDays(int[] nums, int days) {
-        int n = nums.length;
-        int maxi = Integer.MIN_VALUE;
-        int sum = 0;
+        int n = nums.length,maxi = Integer.MIN_VALUE,sum = 0;
         for(int i=0;i<n;i++){
             maxi = Math.max(maxi,nums[i]);
             sum += nums[i];
         }
-
-        int low = maxi;
-        int high = sum;
+        int low = maxi,high = sum;
         while(low<=high){
             int mid = low + (high-low)/2;
-            if(findCapacity(nums,days,mid)){
-                high = mid - 1;
-            }
-            else{
-                low = mid + 1;
-            }
+            if(findCapacity(nums,days,mid)) high = mid - 1;
+            else low = mid + 1;
         }
         return low;
     }
